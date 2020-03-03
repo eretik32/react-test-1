615e44afe1f665d48e1471ba13692f5bdc9f989c
developer32 <eretik32freeplay@yandex.ru>
2020-03-03 21:40:43 +0300

    ver 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Usopen-header-logo.svg/1280px-Usopen-header-logo.svg.png" />
+        </header>
+    )
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://avatars.mds.yandex.net/get-pdb/202366/7220d9c8-5f13-4826-9fd7-62632b5d96f8/s1200" />
+      </div>
+      <div>
+        ava + descr
+      </div>
+      <div>
+        My-post
+        <div>New post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Profile;
